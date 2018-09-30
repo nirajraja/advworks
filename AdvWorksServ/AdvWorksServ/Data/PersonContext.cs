980fcc366fb1846cb3e0bf23f1b1b19dfb9e2167
Justin <jaykh2o@gmail.com>
2018-09-30 08:44:42 -0400

    Added People API

--- AdvWorksServ/AdvWorksServ/Data/PersonContext.cs
+++ AdvWorksServ/AdvWorksServ/Data/PersonContext.cs
@@ -4,15 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace AdvWorksServ.Model
+namespace AdvWorksServ
 {
     public class PersonContext : DbContext
     {
         public PersonContext(DbContextOptions<PersonContext> options) : base(options)
         {
-
+            
         }
 
         public DbSet<Person> People { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().ToTable("Person", "Person");
+        }
     }
 }
